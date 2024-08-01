0189c0a7bf10e9dbb2a9c702674db4c7b66cbef9
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-31 20:44:22 -0700

    Number template

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,14 +7,20 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns as given string"""
+    """returns Hello given string"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns as given string"""
+    """returns HBNB as given string"""
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=None)
